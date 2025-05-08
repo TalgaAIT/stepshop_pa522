8ac8b86074906d05caea188bf09f5cc166f0d823
talgat <talga_ait@mail.ru>
2025-05-08 18:28:36 +0500

    lesson_06

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
+
 def index(request):
     title = 'главная страница'
 
+    prods = Product.objects.all()[:4]
+
     context = {
         'title': title,
+        'products': prods,
     }
 
     return render(request, 'index.html', context)
